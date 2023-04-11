70f9cf724715c814ec0a4a2d94f611c2a03043be
robotdiary <molihua321.dp@gmail.com>
2023-04-11 23:30:19 +0900

    gameview

---
+++ caramelPopcorn/11.24프론트/src/views/GameView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="game-box" style="min-width:700px; max-width:700px">
+    게임을 시작하지
+    <GameActorView/>
+  </div>
+</template>
+
+<script>
+import GameActorView from '@/views/GameActorView'
+export default {
+  name: 'GameView',
+  components: {
+    GameActorView,
+  }
+}
+</script>
+
+<style>
+/* .game-box {
+
+} */
+</style>
